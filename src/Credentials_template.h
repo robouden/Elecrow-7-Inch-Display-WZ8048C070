808cfe829168b06f3b639fb95d176a6ce668c670
robouden <rob@safecast.org>
2024-04-23 21:23:47 +0900

    Added credentials

--- src/Credentials_template.h
+++ src/Credentials_template.h
@@ -4,5 +4,5 @@ char pass[50];
 
 // Your WiFi credentials.
 
-char default_ssid[] = "your_network";
-char default_pass[] = "your_password";
+char default_ssid[] = "Kittywood";
+char default_pass[] = "schatjeyuka";
